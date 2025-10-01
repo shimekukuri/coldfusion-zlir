845b1153508e7b456425f5c511f45c222877debc
HutchinsoJ <hutchinsoj@Cmhmaclap43459.local>
2025-10-01 11:44:07 -0400

    f

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const vii_module = b.dependency("vii", .{ .target = target, .optimize = optimize }).module("vii");
 
-    _ = b.addModule("coldfusion_zlir", .{ .root_source_file = b.path("src/main.zig"), .target = target, .optimize = optimize });
+    _ = b.addModule("coldfusion_zlir", .{ .root_source_file = b.path("src/coldfusion_zlir.zig"), .target = target, .optimize = optimize, .imports = &.{.{ .name = "vii", .module = vii_module }} });
 }
