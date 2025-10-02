b5da7d0ec9d3489395456045dd81445b77b74355
HutchinsoJ <hutchinsoj@Cmhmaclap43459.local>
2025-10-02 17:20:18 -0400

    updated core version

--- src/lexer.zig
+++ src/lexer.zig
@@ -1,3 +1,8 @@
 const plugin_core = @import("plugin_core");
+const LexerInterface = plugin_core.LexerInterface;
+const Token = LexerInterface.Token;
+const createTokenizer = LexerInterface.createTokenizerType;
 
 pub const VERSION = 0;
+
+fn run(x: []u8, y: *usize) Token {}
